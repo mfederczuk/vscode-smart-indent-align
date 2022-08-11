7c4b935cd6cf885d941c50577720aba9c60b07d2
Michael Federczuk <federczuk.michael@protonmail.com>
2022-08-11 22:35:50 +0200

    Add development files

---
+++ src/extension.ts
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2022 Michael Federczuk
+ * SPDX-License-Identifier: MPL-2.0 AND Apache-2.0
+ */
+
+import * as vscode from "vscode";
+
+export function activate(context: vscode.ExtensionContext) {
+	// TODO
+}
