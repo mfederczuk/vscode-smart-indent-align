211865a5ab407e2de4ffc3bee575909673c0f194
Michael Federczuk <federczuk.michael@protonmail.com>
2022-08-11 23:56:29 +0200

    Add newline command

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,33 @@
 
 import * as vscode from "vscode";
 
+const EXTENSION_ID = "smart-indent-align";
+
+const COMMAND_ID_NEWLINE = `${EXTENSION_ID}.newline`;
+
+const newlineSelection = (selection: vscode.Selection, textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit) => {
+	const startLine: vscode.TextLine = textEditor.document.lineAt(selection.start);
+
+	const leadingWhitespace: string = startLine.text
+		.substring(
+			0,
+			Math.min(startLine.firstNonWhitespaceCharacterIndex, selection.start.character),
+		);
+
+	edit.delete(selection);
+	edit.insert(selection.start, "\n" + leadingWhitespace);
+};
+
+const commandNewline = (textEditor: vscode.TextEditor, edit: vscode.TextEditorEdit): void => {
+	textEditor.selections.forEach((selection: vscode.Selection) => {
+		newlineSelection(selection, textEditor, edit);
+	});
+};
+
 export function activate(context: vscode.ExtensionContext) {
-	// TODO
+	console.log(`Activating ${EXTENSION_ID}`);
+
+	const newline = vscode.commands.registerTextEditorCommand(COMMAND_ID_NEWLINE, commandNewline);
+
+	context.subscriptions.push(newline);
 }
